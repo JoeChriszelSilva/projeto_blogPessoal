6a66d03802a0b0aec39a06ae0c68d71e1006a0a9
Joe Chriszel Silva <90300759+JoeChriszelSilva@users.noreply.github.com>
2025-10-28 07:35:00 -0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   process.env.TZ = '-03:00';
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog Pessoal')
+    .setDescription('Projeto Blog Pessoal')
+    .setContact(
+      'Generation Brasil',
+      'http://www.generationbrasil.online',
+      'generation@email.com',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
 
--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 import { Postagem } from '../../postagem/entities/postagem.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'tb_usuarios' })
 export class Usuario {
@@ -9,19 +10,23 @@ export class Usuario {
 
   @IsNotEmpty()
   @Column({ length: 255, nullable: false })
+  @ApiProperty()
   nome: string;
 
   @IsEmail()
   @IsNotEmpty()
   @Column({ length: 255, nullable: false })
+  @ApiProperty({ example: 'email@email.com.br' })
   usuario: string;
 
   @MinLength(8)
   @IsNotEmpty()
   @Column({ length: 255, nullable: false })
+  @ApiProperty()
   senha: string;
 
   @Column({ length: 5000, nullable: true })
+  @ApiProperty()
   foto: string;
 
   @OneToMany(() => Postagem, (postagem) => postagem.usuario)
