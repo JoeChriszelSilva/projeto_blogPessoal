56c7da514d4138d7ab636c403af2af1d07a9f4d7
Joe Chriszel Silva <90300759+JoeChriszelSilva@users.noreply.github.com>
2025-11-03 02:48:56 -0300

    feat: corrigindo injecao de config para deploy

--- src/data/service/prod.service.ts
+++ src/data/service/prod.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Postagem } from 'src/postagem/entities/postagem.entity';
+import { Tema } from 'src/Tema/entities/tema.entity';
+import { Usuario } from 'src/usuario/entities/usuario.entity';
 
 @Injectable()
 export class ProdService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      url: process.env.DATABASE_URL,
+      url: this.configService.get<string>('DATABASE_URL'),
       logging: false,
       dropSchema: false,
+      entities: [Postagem, Tema, Usuario],
       ssl: {
         rejectUnauthorized: false,
       },
